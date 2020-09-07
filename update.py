4f5099f102144c9ed1058a737586804476e31845
teamclouday <teamclouday@gmail.com>
2020-09-07 02:30:47 -0400

    update script

--- update.py
+++ update.py
@@ -1,4 +1,26 @@
 # a script to update main page (recent work and recent blog)
 
-if __name__=="__main__":
+import os
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+def update_about():
+    """
+    Update the 'Last Update' date in About subpage
+    """
+    with open(os.path.join("subpage", "about", "index.html"), "r") as inFile:
+        html = BeautifulSoup(inFile.read(), "lxml")
+    html.find('div', {'class':'last_update'}).string = "Last Update: {}".format(datetime.date(datetime.now()))
+    with open(os.path.join("subpage", "about", "index.html"), "wb") as outFile:
+        outFile.write(html.prettify('utf-8'))
+
+def update_blogs():
     pass
+
+def update_main():
+    pass
+
+if __name__=="__main__":
+    update_about()
+    update_blogs()
+    update_main()
