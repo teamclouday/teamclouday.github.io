28a0bf4290e45369281b9ed0af6e76e058ebf5fc
teamclouday <teamclouday@gmail.com>
2020-09-14 00:32:16 -0400

    update script

--- update.py
+++ update.py
@@ -5,7 +5,7 @@
 
 import os
 from datetime import datetime
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 
 def update_about():
     """
@@ -18,7 +18,49 @@ def update_about():
         outFile.write(html.prettify('utf-8'))
 
 def update_blogs():
-    pass
+    """
+    Update the recently added blogs
+    """
+    with open(os.path.join("subpage", "blogs", "index.html"), "r") as inFile:
+        blog = BeautifulSoup(inFile.read(), "lxml")
+    blogMainBody = blog.find('div', {'id':'page_body'})
+    # define template
+    template = """
+    <a tag="{0}" href="{0}.html" style="text-decoration: none;">
+      <div class="blog">
+        <div class="title">{1}</div>
+        <div class="abstract">{2}</div>
+      </div>
+    </a>
+    """
+    # loop the directory
+    for filename in os.listdir(os.path.join("subpage", "blogs")):
+        if filename.endswith("html") and filename != "index.html" and filename != "template.html":
+            # search the date
+            date = os.path.splitext(filename)[0]
+            found = False
+            for child in blogMainBody.findChildren("a", recursive=False):
+                if child["tag"] == date:
+                    found = True
+                    break
+            if not found:
+                # retrieve information
+                with open(os.path.join("subpage", "blogs", filename), "r") as inFile:
+                    currentBlog = BeautifulSoup(inFile.read(), "lxml")
+                title = Tag(builder=blog.builder, name="div", attrs={"class":"title"})
+                title.string = currentBlog.find("title").string
+                abstract = Tag(builder=blog.builder, name="div", attrs={"class":"abstract"})
+                abstract.string = currentBlog.find("div", {"class": "normal"}).text.strip().replace("\n", " ") + "... [Click to see more]"
+                # insert newData
+                newData0 = Tag(builder=blog.builder, name="a", attrs={"tag":date, "href":"{}.html".format(date), "style": "text-decoration: none;"})
+                newData1 = Tag(builder=blog.builder, name="div", attrs={"class":"blog"})
+                newData1.insert(0, title)
+                newData1.insert(1, abstract)
+                newData0.insert(0, newData1)
+                blogMainBody.insert(0, newData0)
+    # finally, update the file
+    with open(os.path.join("subpage", "blogs", "index.html"), "wb") as outFile:
+        outFile.write(blog.prettify('utf-8'))
 
 def update_main():
     pass
