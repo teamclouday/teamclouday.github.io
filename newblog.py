282f760560ff5f004b8c3807b08dac402d0ce330
teamclouday <teamclouday@gmail.com>
2020-09-14 00:12:44 -0400

    update blog adding script

--- newblog.py
+++ newblog.py
@@ -11,8 +11,10 @@ if __name__=="__main__":
     # get current time
     currentTime = datetime.now().strftime("%Y_%m_%d_%H_%M")
     if not os.path.exists(os.path.join("subpage", "blogs", "{}.html".format(currentTime))):
+        html.find('div', {'class':'last_update'}).string = "Last Update: {}".format(datetime.now().strftime("%Y-%m-%d %H:%M"))
         with open(os.path.join("subpage", "blogs", "{}.html".format(currentTime)), "wb") as outFile:
             outFile.write(html.prettify('utf-8'))
+    # update blog time
     # create assets folder
     if not os.path.exists(os.path.join("subpage", "blogs", currentTime)):
         os.makedirs(os.path.join("subpage", "blogs", currentTime))
